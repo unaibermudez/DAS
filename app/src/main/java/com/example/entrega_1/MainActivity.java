971ee62a6221612f11923b3995c92a37995fd7a2
Unai Bermudez <unaberosa@gmail.com>
2024-03-09 16:53:02 +0100

    commit prueba

--- app/src/main/java/com/example/entrega_1/MainActivity.java
+++ app/src/main/java/com/example/entrega_1/MainActivity.java
@@ -16,6 +16,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    //test commit
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
